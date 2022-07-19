8a88baaee8e2c86306ee5fe681304681baea16cf
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-07-19 13:04:09 +0100

    Fix Rubocop issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_19_112715) do
+ActiveRecord::Schema[7.0].define(version: 20_220_719_112_715) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "greetings", force: :cascade do |t|
-    t.string "greeting"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'greetings', force: :cascade do |t|
+    t.string 'greeting'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
